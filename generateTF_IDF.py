6fb9b8f22732e4b403379a65af1a16b3a5dc3aa0
soyabansari <ansari_831957@student.nitw.ac.in>
2023-07-25 00:09:39 +0530

    first commit

---
+++ generateTF_IDF.py
@@ -0,0 +1,153 @@
+# titles
+
+import math
+import string
+import re
+import pandas as pd
+import numpy as np
+
+from gensim.parsing.preprocessing import remove_stopwords
+
+
+keywords = []
+
+sentence = []
+
+for cnt in range(0, 965):
+    f1 = open("./Problems/P_" + str(cnt + 1) + ".txt", encoding="utf-8")
+    docs = str(f1.read())
+
+    docs = docs.replace("\\n", " ")
+    documents_clean = []
+
+    # Remove Mentions
+    document_test = re.sub(r"@\w+", " ", docs)
+
+    # Lowercase the document
+    document_test = document_test.lower()
+
+    # Remove punctuations
+    document_test = re.sub(r"[%s]" % re.escape(string.punctuation), " ", document_test)
+
+    #  the numbers
+    document_test = re.sub(r"[0-9]", " ", document_test)
+
+    # Remove the doubled space
+    document_test = re.sub(r"\s{2,}", " ", document_test)
+    documents_clean.append(document_test)
+
+    filtered_sentence = remove_stopwords(documents_clean[0])
+
+    # print(filtered_sentence)
+    filtered_sentence = sorted(filtered_sentence.split(" "))
+
+    # print(filtered_sentence)
+    sentence.append(filtered_sentence)
+
+    filtered_sentence = set(filtered_sentence)
+    # print(filtered_sentence)
+    for i in filtered_sentence:
+        keywords.append(i)
+# print(len(sentence))
+keywords = sorted(set(keywords))
+
+f1 = open("./Keywords.txt", "w+")
+f1.write("\n".join(keywords))
+
+
+# Calculating TF
+
+TF = []
+for i in range(len(sentence)):
+    no_of_keywords_local = len(sentence[i])
+
+    for j in range(len(keywords)):
+        cnt = sentence[i].count(keywords[j])
+        if cnt == 0:
+            continue
+        tf_local = []
+        tf_local.append(i)
+        tf_local.append(j)
+        tf_local.append(cnt / no_of_keywords_local)
+        TF.append(tf_local)
+
+
+# Calculating IDF
+
+IDF = []
+
+N = len(sentence)
+
+
+counts = []
+for i in range(len(keywords)):
+    counts.append(0)
+
+
+for i in range(len(TF)):
+    counts[TF[i][1]] += 1
+
+
+for i in range(len(keywords)):
+    IDF.append((1 + math.log(N / counts[i])))
+
+
+f1 = open("./IDF.txt", "w+")
+ToAdd = ""
+
+for i in IDF:
+    ToAdd += str(i)
+    ToAdd += "\n"
+f1.write(ToAdd)
+
+
+# Calculating Importance Matrix (TFIDF Matrix)
+
+Importance_Matrix = []
+
+
+for i in range(len(TF)):
+    Imp_Matrix = []
+    Imp_Matrix.append(TF[i][0])
+    Imp_Matrix.append(TF[i][1])
+    Imp_Matrix.append(TF[i][2] * IDF[TF[i][1]])
+
+    Importance_Matrix.append(Imp_Matrix)
+
+
+f1 = open("./TFIDF.txt", "w+")
+ToAdd = ""
+
+for i in range(len(Importance_Matrix)):
+    ToAdd += str(Importance_Matrix[i][0])
+    ToAdd += " "
+    ToAdd += str(Importance_Matrix[i][1])
+    ToAdd += " "
+    ToAdd += str(Importance_Matrix[i][2])
+    ToAdd += "\n"
+f1.write(ToAdd)
+
+# Calculate Magnitude of the vector
+
+Magnitude = []
+
+for i in range(len(sentence)):
+    Magnitude.append(0.0)
+
+for i in range(len(Importance_Matrix)):
+    Magnitude[Importance_Matrix[i][0]] += (
+        Importance_Matrix[i][2] * Importance_Matrix[i][2]
+    )
+
+for i in range(len(Magnitude)):
+    Magnitude[i] = math.sqrt(Magnitude[i])
+
+
+f1 = open("./Magnitude.txt", "w+")
+ToAdd = ""
+
+for i in Magnitude:
+    ToAdd += str(i)
+    ToAdd += "\n"
+f1.write(ToAdd)
+f1.close()
---
+++ index.js
@@ -0,0 +1,212 @@
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const fs = require("fs");
+const sw = require("stopword");
+const { removeStopwords } = require("stopword");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+const PORT = process.env.PORT || 3000;
+
+// Reading titles
+titles = [];
+let data = fs.readFileSync("./Problems/Problem_Titles.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+titles = data.split(/\r?\n/);
+
+// Reading URLs
+URLs = [];
+data = fs.readFileSync("./Problems/Problem_URLs.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+URLs = data.split(/\r?\n/);
+
+// Reading Magnitude
+
+Magnitude = [];
+data = fs.readFileSync("./Magnitude.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+Magnitude = data.split(/\r?\n/);
+Magnitude.splice(-1);
+
+// Reading Keywords
+
+Keywords = [];
+
+data = fs.readFileSync("./Keywords.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+Keywords = data.split(/\r?\n/);
+
+// console.log(Keywords);
+
+// Reading IDF
+
+IDF = [];
+data = fs.readFileSync("./IDF.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+IDF = data.split(/\r?\n/);
+IDF.splice(-1);
+
+// Reading TFIDF (Importance Matrix)
+
+Importance_Matrix = [];
+data = fs.readFileSync("./TFIDF.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+Importance_Matrix = data.split(/\r?\n/);
+Importance_Matrix.splice(-1);
+
+for (let i = 0; i < Importance_Matrix.length; i++) {
+  Importance_Matrix[i] = Importance_Matrix[i].split(" ");
+}
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/search", (req, res) => {
+  const query = req.query;
+
+  let question = query.question;
+
+  // // Tf IDF algo
+
+  // Query Keywords
+
+  let query_keywords = [];
+  question = question.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  question = question.toLowerCase();
+  query_keywords = question.split(" ");
+  query_keywords = removeStopwords(query_keywords);
+  // for (let i = 0; i < query_keywords.length; i++) {
+  //   query_keywords[i] = lemmatizer(query_keywords[i]);
+  // }
+  query_keywords = query_keywords.sort();
+  // console.log(query_keywords);
+
+  // Query TF
+  let query_TF = [];
+
+  for (let i = 0; i < Keywords.length; i++) {
+    let cnt = query_keywords.filter((x) => x === Keywords[i]).length;
+    if (cnt == 0) continue;
+
+    tf_local = [];
+    tf_local.push(0);
+    tf_local.push(i);
+    tf_local.push(cnt / query_keywords.length);
+    query_TF.push(tf_local);
+  }
+  // console.log(query_TF);
+
+  // Query Importance Matrix
+
+  let query_importance_matrix = [];
+
+  for (let i = 0; i < query_TF.length; i++) {
+    Imp_Mat = [];
+    Imp_Mat.push(query_TF[i][0]);
+    Imp_Mat.push(query_TF[i][1]);
+    Imp_Mat.push(query_TF[i][2] * parseFloat(IDF[query_TF[i][1]]));
+    query_importance_matrix.push(Imp_Mat);
+  }
+  // console.log(query_importance_matrix);
+
+  let query_magnitude = 0.0;
+
+  for (let i = 0; i < query_importance_matrix.length; i++) {
+    query_magnitude +=
+      query_importance_matrix[i][2] * query_importance_matrix[i][2];
+  }
+
+  query_magnitude = Math.sqrt(query_magnitude);
+  // console.log(query_magnitude);
+
+  let similarity = [];
+
+  for (let i = 0; i < Magnitude.length; i++) {
+    sim = [];
+    sim.push(0.0);
+    sim.push(i);
+    similarity.push(sim);
+  }
+
+  for (let i = 0; i < query_importance_matrix.length; i++) {
+    let toCheckKwd = query_importance_matrix[i][1];
+    for (let j = 0; j < Importance_Matrix.length; j++) {
+      if (parseInt(Importance_Matrix[j][1]) == toCheckKwd) {
+        similarity[parseInt(Importance_Matrix[j][0])][0] +=
+          query_importance_matrix[i][2] * parseFloat(Importance_Matrix[j][2]);
+      }
+    }
+  }
+
+  for (let i = 0; i < Magnitude.length; i++) {
+    similarity[i][0] =
+      similarity[i][0] / (parseFloat(Magnitude[i]) * query_magnitude);
+  }
+  similarity = similarity.sort().reverse();
+
+  // List of top 5 questions
+  arr = [];
+
+  for (let i = 0; i < 5; i++) {
+    ques_details = [];
+    ques_no = similarity[i][1];
+    ques_details.push(titles[ques_no]);
+    ques_details.push(URLs[ques_no]);
+
+    fileData = fs.readFileSync(
+      "./Problems/P_" + (ques_no + 1).toString() + ".txt",
+      { encoding: "utf-8", flag: "r" }
+    );
+    fileData = fileData.replace(/\\n/g, " ");
+
+    if (fileData.length > 100) ques_details.push(fileData.slice(2, 100));
+    else ques_details.push(fileData.slice(2, -1));
+    arr.push(ques_details);
+  }
+
+  
+  
+  res.json(arr);
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>ABOUT PAGE</h1>");
+});
+
+app.get("/question/:id", (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  res.locals.question_name = titles[id - 1];
+  res.locals.question_url = URLs[id - 1];
+  res.render("question");
+});
+
+app.listen(PORT, () => {
+  console.log("I am on PORT no. " + PORT);
+});
